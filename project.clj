154481f23b3170a3fa4b55a4238e1233c7531386
tuddman <scott.tudd@gmail.com>
2019-06-13 20:35:20 -0600

    bump version to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject socketcluster-clj "0.1.0-SNAPSHOT"
+(defproject tuddman/socketcluster-clj "0.1.0"
   :description "socketcluster-clj : a SocketCluster Client library for Clojure"
   :url "https://socketcluster.io"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
